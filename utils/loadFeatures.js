520565b4be3401c221d681595f4518ee5af0d04b
theTWIXhunter <147813783+theTWIXhunter@users.noreply.github.com>
2025-12-26 19:47:50 +0100

    add debug logs

--- utils/loadFeatures.js
+++ utils/loadFeatures.js
@@ -11,7 +11,13 @@ function loadFeatures(featuresDir) {
     throw new Error(`Features directory does not exist: ${featuresDir}`);
   }
   const featureFiles = fs.readdirSync(featuresDir).filter(f => f.endsWith('.js'));
-  return featureFiles.map(f => require(path.join(featuresDir, f)));
+  console.log('[Loader] Loading features:', featureFiles);
+  const loaded = featureFiles.map(f => {
+    const mod = require(path.join(featuresDir, f));
+    console.log(`[Loader] Loaded feature: ${f}`);
+    return mod;
+  });
+  return loaded;
 }
 
 module.exports = loadFeatures;
